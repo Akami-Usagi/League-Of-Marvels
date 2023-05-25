2df14165d525f9591f241b80aa709841b4736338
Camilo Arango <105063817+Akami-Usagi@users.noreply.github.com>
2023-05-25 06:05:27 -0500

    cambio a guardianes de la galaxia

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const getCharacters = () => {
   const timestamp = new Date().getTime();
   const hash = md5(timestamp + API_PRIVATE_KEY + API_PUBLIC_KEY);
 
-  const url = `${BASE_URL}/characters?nameStartsWith=cap&ts=${timestamp}&apikey=${API_PUBLIC_KEY}&hash=${hash}`;
+  const url = `${BASE_URL}/series/16410/characters?offset=2&ts=${timestamp}&apikey=${API_PUBLIC_KEY}&hash=${hash}`;
 
   return axios.get(url);
 };
